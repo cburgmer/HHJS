2cbb1f73d16eb085d974364049f38bbe32a706a0
Christoph Burgmer <cburgmer@ira.uka.de>
2013-12-16 21:05:30 +0100

    First proper promise

--- promises/promises.js
+++ promises/promises.js
@@ -1,5 +1,20 @@
-module.exports.fulfilledPromise = {
-    then: function (callback) {
-        callback();
-    }
+module.exports.defer = function () {
+    var resolved, resolveCallback;
+    return {
+        resolve: function () {
+            resolved = true;
+            if (resolveCallback) {
+                resolveCallback();
+            }
+        },
+        promise: {
+            then: function (callback) {
+                if (resolved) {
+                    callback();
+                } else {
+                    resolveCallback = callback;
+                }
+            }
+        }
+    };
 };
--- promises/test/promisesSpec.js
+++ promises/test/promisesSpec.js
@@ -8,8 +8,34 @@ describe('promises', function () {
     });
 
     it('should execute handler', function () {
-        promises.fulfilledPromise.then(callback);
+        var defer = promises.defer();
+        defer.resolve();
+        defer.promise.then(callback);
 
         expect(callback).toHaveBeenCalled();
+    });
+
+    it('should not execute handler if not resolved', function () {
+        var defer = promises.defer();
+        defer.promise.then(callback);
+
+        expect(callback).not.toHaveBeenCalled();
+    });
+
+    it('should execute handler (after resolve)', function () {
+        var defer = promises.defer();
+        defer.promise.then(callback);
+        defer.resolve();
+
+        expect(callback).toHaveBeenCalled();
+    });
+
+    it('should execute only once', function () {
+        var defer = promises.defer();
+        defer.resolve();
+        defer.promise.then(callback);
+        defer.resolve();
+
+        expect(callback.callCount).toEqual(1);
     });
 });
